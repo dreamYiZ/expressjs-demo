05e6f65ebfaf9e8d8b52f61dbbc6620ebe443c3f
Ubuntu <ubuntu@ip-172-31-7-53.us-east-2.compute.internal>
2020-11-21 19:51:24 +0000

    add log

--- app.js
+++ app.js
@@ -3,6 +3,29 @@ const path = require('path')
 
 const app = express()
 const port = 3000
+const fs = require('fs');
+
+
+function initFiles(){
+  var dir = './log';
+  if (!fs.existsSync(dir)){
+      fs.mkdirSync(dir);
+  }
+}
+
+var requestTime = function (req, res, next) {
+  req.requestTime = new Date();
+  fs.writeFile('log/req.log', `Time:${req.requestTime},Url:${ req.protocol}://${req.get('host')}}`, function (err) {
+    if (err) return console.log(err);
+  });
+  next()
+}
+
+
+initFiles();
+
+app.use(requestTime)
+
 
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
@@ -18,6 +41,13 @@ app.post('/post', function (req, res) {
   res.send('Got a POST request')
 })
 
-app.listen(port, '0.0.0.0',() => {
-  console.log(`Example app listening at http://0.0.0.0:${port}`)
-})
+
+
+
+
+/* istanbul ignore next */
+if (!module.parent) {
+  app.listen(port, '0.0.0.0',() => {
+    console.log(`Example app listening at http://0.0.0.0:${port}`)
+  })
+}
